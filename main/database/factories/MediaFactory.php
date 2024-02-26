426e0d632f373dde13c01779115b43c7a185b281
ragg09 <ragunayon@gmail.com>
2024-02-26 14:41:45 +0800

    add seeder

---
+++ main/database/factories/MediaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Media>
+ */
+class MediaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'link' => $this->faker->url,
+            'file' => 'sample.jpg',
+            'type' => 'image',
+        ];
+    }
+}
